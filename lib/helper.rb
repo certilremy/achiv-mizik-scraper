373f38952fab6e3658b15c19708cd90e9aec43de
certilremy <certilremy@gmail.com>
2020-02-19 10:23:41 -0500

    added the choice validator for the menu

---
+++ lib/helper.rb
@@ -0,0 +1,21 @@
+module MenuHelper
+  def conver(choice)
+    choice.to_i - 1
+  end
+
+  def valide_choice(input)
+    if input == ' '
+      false
+
+    elsif input == ''
+
+    elsif !input.between?(1, 10)
+
+      false
+
+    else
+      true
+    end
+  end
+end
+
