84f7a6477f685d67478c29699e70b8365cfbc4f2
certilremy <certilremy@gmail.com>
2020-02-19 23:37:49 -0500

    added test for Music

---
+++ spec/music_spec.rb
@@ -0,0 +1,44 @@
+require_relative '../lib/musics.rb'
+
+describe 'Music' do
+  describe '#Initializer' do
+    let(:music) { Music.new }
+    it 'Create a  new music with empty title' do
+      expect(music.title).to eq(nil)
+    end
+    it 'Create a  new music with empty link' do
+      expect(music.link).to eq(nil)
+    end
+
+    it 'Assingn  a  new title for the music' do
+      music.title = 'fantom'
+      expect(music.title).to eq('fantom')
+    end
+    it 'Assingn  a  new link for the music' do
+      music.title = 'https://achvimizik.net'
+      expect(music.title).to eq('https://achvimizik.net')
+    end
+  end
+
+  context 'class methods' do
+    describe '.all ' do
+      it 'returns an array of all the instances of the Music class' do
+        Music.reset
+        music_one = Music.new
+        music_two = Music.new
+        music_three = Music.new
+        expect(Music.all).to match_array([music_one, music_two, music_three])
+      end
+    end
+  end
+
+  context 'class methods' do
+    describe '.Single-Music ' do
+      it 'returns a single instances of the Music class' do
+        Music.reset
+        Music.new('fantom', 'link')
+        expect(Music.all[0].title).to eq('fantom')
+      end
+    end
+  end
+end
